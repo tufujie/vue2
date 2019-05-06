2e5b7ed5e02336611ed3bcf2680dd7061515f2eb
Jef <jef.tu@foxmail.com>
2019-05-06 15:11:41 +0800

    first commit

---
+++ src/component/firstcomponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="firstcomponent">
+    <a> author by {{ author }} </a>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data () {
+      return {
+        author: "Jef"
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
