cb25afc83386d5eeddfae63e39685894c629b066
Jef <jef.tu@foxmail.com>
2019-06-12 21:07:53 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-gouzi="http://www.w3.org/1999/xhtml">
   <div id="app">
     <img src="./assets/logo.png">
     <!-- 插值之文本，数据绑定最常见的形式就是使用 {{...}}（双大括号）的文本插值 -->
@@ -196,9 +196,32 @@
       <site-item v-for="item in sites" v-bind:siteItem="item"></site-item>
     </ol>
     <validateComponent v-bind:validateNum="123"></validateComponent>
+    <h2>自定义事件组件</h2>
+    <p>{{ total }}</p>
+    <!-- 每次触发，本身+1，total加1，所以total是全部之和 -->
+    <button-counter v-on:increment="incrementTotal"></button-counter>
+    <button-counter v-on:increment="incrementTotal"></button-counter>
+    <button-counter2></button-counter2>
+    <button-counter2></button-counter2>
+    <h1>Vue.js 自定义指令</h1>
+    <p v-gouzi:hello.a.b="message">
+    </p>
+    <p v-gouziV2="{color: 'blue', text: 'Hello Jef'}">
+    </p>
+    <h1>Vue.js 路由</h1>
+    <p>
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <v-link href="/">Home</v-link>
+      <v-link href="/abount">About</v-link>
+    </p>
 
 
 
+
+    <br/>
+    <input v-focus>
     <br/>
     <helloCompnent message="Hello Vue!"></helloCompnent>
     <authorComponent></authorComponent>
@@ -274,7 +297,8 @@
         picked: "Tecent",
         selected: '',
         age: 18,
-        parentMsg: '父组件内容'
+        parentMsg: '父组件内容',
+        total: 0
       }
     },
     methods: {
@@ -313,6 +337,9 @@
       },
       clear: function () {
         this.userName = '';
+      },
+      incrementTotal: function () {
+        this.total += 1
       }
     },
     filters: {
@@ -406,6 +433,74 @@
       }
     },
     template: '<input type="text" name="validateText" v-bind:validateNum="123">'
+  });
+  // 自定义事件组件，用于计数
+  Vue.component('button-counter', {
+    template: '<button v-on:click="incrementHandler">{{ counter }}</button>',
+    data: function () {
+      return {
+        // data 选项不是一个对象，不会影响到其他实例
+        counter: 0
+      }
+    },
+    methods: {
+      incrementHandler: function () {
+        this.counter += 1
+        this.$emit('increment')
+      }
+    },
+  });
+  var buttonCounter2Data = {
+    count: 0
+  };
+  Vue.component('button-counter2', {
+    data: function () {
+      // data 选项是一个对象，会影响到其他实例
+      return buttonCounter2Data;
+    },
+    template: '<button v-on:click="count++">点击了 {{ count }} 次。</button>'
+  });
+  // 注册一个全局自定义指令 v-focus
+  Vue.directive('focus', {
+    // 当绑定元素插入到 DOM 中。
+    inserted: function (el) {
+      // 聚焦元素
+      el.focus()
+    }
+  });
+  // 注册局部指令
+ /* new Vue({
+    el: '#app',
+    directives: {
+      // 注册一个局部的自定义指令 v-focus
+      focus: {
+        // 指令的定义
+        inserted: function (el) {
+          // 聚焦元素
+          el.focus()
+        }
+      }
+    }
+  });*/
+  Vue.directive('gouzi', {
+    bind: function (el, binding, vnode) {
+      var s = JSON.stringify
+      el.innerHTML =
+        'name: '       + s(binding.name) + '<br>' +
+        'expression: ' + s(binding.expression) + '<br>' +
+        'value: '      + s(binding.value) + '<br>' +
+        'argument: '   + s(binding.arg) + '<br>' +
+        'modifiers: '  + s(binding.modifiers) + '<br>' +
+        'vnode keys: ' + Object.keys(vnode).join(', ');
+      el.style.backgroundColor = binding.value.color;
+    }
+  });
+  Vue.directive('gouziV2', {
+    bind: function (el, binding) {
+      // 简写方式设置文本及背景颜色
+      el.innerHTML = binding.value.text;
+      el.style.backgroundColor = binding.value.color;
+    }
   });
 </script>
 <style>
